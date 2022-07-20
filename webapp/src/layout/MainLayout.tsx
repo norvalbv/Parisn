f285b40f6d7a11d64c8f0294f9137676abfabe3e
Benji <benji@beamconnectivity.com>
2022-07-20 19:25:08 +0100

    minor improvements

--- webapp/src/layout/MainLayout.tsx
+++ webapp/src/layout/MainLayout.tsx
@@ -1,5 +1,25 @@
-const MainLayout = () => {
-  return <div>MainLayout</div>;
-};
+import React, { ReactElement } from "react";
+import { Outlet } from "react-router-dom";
+import CardWrapper from "../../components/CardWrapper";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+// ==============================|| MAIN LAYOUT ||============================== //
+
+/**
+ * Outside login screen layout
+ * Header and Sidebar
+ */
+const MainLayout = (): ReactElement => (
+  <div className="flex max-h-screen">
+    <div className="overflow-auto w-full">
+      <CardWrapper className="mx-8 my-8 w-full flex flex-col relative">
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </CardWrapper>
+    </div>
+  </div>
+);
 
 export default MainLayout;
