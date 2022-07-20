b07c0b7ae1a93daaffdd6833a5b1607f3b1a0005
Benji <benji@beamconnectivity.com>
2022-07-20 22:33:08 +0100

    add components, page

--- webapp/src/layout/Footer.tsx
+++ webapp/src/layout/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Footer = () => {
-  return <div>Footer</div>;
+const NavBar = () => {
+  return (
+    <ul className="flex text-white justify-end items-center pr-8 py-6 z-50">
+      <li className="px-10 cursor-pointer hover:border-b">How It Works</li>
+      <li className="px-10 cursor-pointer hover:border-b">
+        Terms And Conditions
+      </li>
+      <li className="px-10 cursor-pointer hover:border-b">Privacy Policy</li>
+    </ul>
+  );
 };
 
-export default Footer;
+export default NavBar;
---
+++ webapp/src/pages/ItemView.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const ItemView = () => {
+  const [clicked, setClicked] = useState(false);
+  return <div>ItemView</div>;
+};
+
+export default ItemView;
