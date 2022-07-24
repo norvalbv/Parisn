43b942a61626fa12eeaca9c6a5577c58ee207243
Benji <benji@beamconnectivity.com>
2022-07-24 13:00:24 +0100

    Feat: Added page templates, Privacy policy, about us, terms and conditions, and how it works

---
+++ webapp/src/pages/AboutUs.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex iste ratione
+      beatae hic saepe provident temporibus, id repellendus. Veniam ratione illo
+      consectetur cupiditate quisquam eaque delectus ipsa assumenda sed dolorum
+      et, numquam nihil voluptas aut corrupti explicabo molestiae, doloribus
+      aperiam placeat quasi debitis nemo. Modi inventore, vel commodi voluptate
+      accusantium aliquam fugit ipsam illo unde. Quasi vero quo similique ex
+      maiores commodi unde, voluptas distinctio corrupti iste ducimus laboriosam
+      saepe impedit dicta dolorum, soluta quae, harum consectetur! Nostrum fugit
+      vero sequi quam, corporis unde libero dicta, itaque deserunt iure soluta
+      nihil quibusdam excepturi suscipit repellat, placeat hic. Recusandae,
+      voluptatem beatae!
+    </p>
+  );
+};
+
+export default AboutUs;
---
+++ webapp/src/pages/HowItWorks.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HowItWorks = () => {
+  return (
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+      fugiat vel doloribus nesciunt! Non odit reprehenderit placeat sint tempore
+      fugit animi nemo porro quod ipsa, nisi provident quibusdam ipsum a
+      ratione? Assumenda perspiciatis officia sed, hic vero maxime, vel dolor
+      facilis asperiores tempora ratione ipsam fuga numquam aut deserunt odio!
+    </p>
+  );
+};
+
+export default HowItWorks;
