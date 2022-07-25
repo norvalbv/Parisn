e18193feb9d915b663d33d3226d9c2cf314dfa7a
Benji <benji@beamconnectivity.com>
2022-07-25 17:49:39 +0100

    refactir

--- webapp/src/Utils/LiveViewers.tsx
+++ webapp/src/Utils/LiveViewers.tsx
@@ -7,14 +7,9 @@ let socket = io('ws://localhost:8000', {
 
 const LiveViewers = () => {
   const [viewCount, setViewCount] = useState(0);
-
   socket.on('get viewers', (amount) => setViewCount(amount));
 
-  return (
-    <div>
-      <p>Live viewers: {viewCount}</p>
-    </div>
-  );
+  return <p>Live viewers: {viewCount}</p>;
 };
 
 export default LiveViewers;
