7ee48332a5f696af1d02999d258b0a69032b8adb
Benji <benji@beamconnectivity.com>
2022-08-07 23:59:08 +0100

    fixes

--- webapp/src/constants/index.js
+++ webapp/src/constants/index.js
@@ -1,5 +1,8 @@
 // Dashboard
 export const DASHBOARD_IMAGE = 'http://127.0.0.1:5173/images/dashboard-image.jpg';
 
-// Products
+// Products Images
 export const PRODUCT_1_IMAGE = 'http://127.0.0.1:5173/images/shoe.jpg';
+
+// Products Videos
+export const PRODUCT_1_VIDEOS = 'http://127.0.0.1:5173/images/shoe.mp4';
