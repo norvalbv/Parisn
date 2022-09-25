85aa2742d2e3dccc0bc57606488f62e66b98b8b9
Benji <benji@beamconnectivity.com>
2022-09-25 18:31:16 +0100

    add types for backend

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,32 @@
 const app = require('express')();
-// const server = require('http').createServer(app);
+const server = require('http').createServer(app);
 const axios = require('axios');
 const cors = require('cors');
-// const io = require('socket.io')(server, {
-//   cors: {
-//     origin: 'http://127.0.0.1:5173',
-//     credentials: true,
-//   },
-// });
-const data = require('./mockdata.json');
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://127.0.0.1:5173',
+    credentials: true,
+  },
+});
+
+export interface Stock {
+  small: number;
+  medium: number;
+  large: number;
+  extralarge: number;
+}
+export interface MockData {
+  [key: string]: {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    price: number;
+    stock: Stock;
+  };
+}
+
+const data: MockData = require('./mockdata.json');
 
 // environment variables
 const PORT = process.env.PORT || 8000;
@@ -24,19 +42,33 @@ app.get('/products/:productid', (req: any, res: any) => {
 });
 
 // socket.io functions
-// io.on('connect', function (socket: any) {
-//   socket.emit('get viewers', io.engine.clientsCount);
+io.on('connect', function (socket: any) {
+  socket.on('join room', async (param: string, callback: any) => {
+    console.log(param);
+    socket.join(param);
 
-//   // console.log(socket.handshake);
-//   socket.join('room 237');
+    callback(io.engine.clientsCount);
+  });
 
-//   socket.join(['room 237', 'room 238']);
+  socket.on('get room', async (param: string, callback: any) => {
+    console.log(param);
 
-//   io.to('room 237').emit('a new user has joined the room'); // broadcast to everyone in the room
-// });
+    console.log(
+      io
+        .in(param)
+        .fetchSockets()
+        .then((room: any) => {
+          console.log('clients in this room:' + param, room.length);
+        })
+    );
 
-// start listening
-app.listen(PORT, () => {
+    callback(io.engine.clientsCount);
+
+    // io.to(param).emit('get viewers', io.engine.clientsCount); // broadcast to everyone in the room
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
