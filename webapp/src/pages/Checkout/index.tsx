4b0478df7974befeb7976a9d68c1e89b0d8cbc6d
Benji <benji@beamconnectivity.com>
2022-10-09 22:06:23 +0100

    add basic checkout

--- webapp/src/pages/Checkout/index.tsx
+++ webapp/src/pages/Checkout/index.tsx
@@ -4,26 +4,37 @@ import useProduct from '../../hooks/useProduct';
 
 const Checkout = () => {
   const { productInfo } = useProduct();
-  console.log(productInfo);
 
   const truthyDataParsed = productInfo && Object.values(productInfo || '').every((item) => item);
 
-  if (!productInfo || !truthyDataParsed) return <></>;
+  if (!productInfo) return <></>;
 
   return (
     <div className="h-screen">
-      <img src={productInfo.product?.image} />
-      {productInfo.price}
-      {productInfo.product?.title}
-      {truthyDataParsed && (
-        <div>
-          <Button
-            text="Clear shopping Basket"
-            onClick={() => {
-              localStorage.clear();
-              window.location.reload();
-            }}
-          />
+      {truthyDataParsed ? (
+        <>
+          <img src={productInfo.product?.image} />
+          <div>
+            {productInfo.price}
+            {productInfo.product?.title}
+            {truthyDataParsed && (
+              <div>
+                <Button
+                  text="Clear shopping Basket"
+                  onClick={() => {
+                    localStorage.clear();
+                    window.location.reload();
+                  }}
+                  navigateTo="/"
+                />
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        <div className="h-screen w-full flex flex-col gap-4 justify-center items-center text-xl">
+          Basket Empty
+          <Button text="Continue Shopping" navigateTo="/catalogue" />
         </div>
       )}
     </div>
