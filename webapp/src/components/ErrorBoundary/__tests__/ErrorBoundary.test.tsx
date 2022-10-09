83ae28f54ee9d0f7e283ce886ce54bcad30584f9
Benji <benji@beamconnectivity.com>
2022-10-09 15:31:48 +0100

    add react error boundary component and test

---
+++ webapp/src/components/ErrorBoundary/__tests__/ErrorBoundary.test.tsx
@@ -0,0 +1,50 @@
+import React, { FC, ReactElement } from 'react';
+import renderer from 'react-test-renderer';
+
+import ErrorBoundary, { ErrorBoundaryProps } from '..';
+
+const ErrorBoundaryWithAllProps: FC<Required<ErrorBoundaryProps>> = ErrorBoundary;
+
+const Child = (): ReactElement => {
+  throw new Error();
+};
+
+describe('<ErrorBoundary />', () => {
+  test('should not render, the div inside should throw an error.', () => {
+    const tree = renderer
+      .create(
+        <ErrorBoundary>
+          <div>Child component</div>
+        </ErrorBoundary>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should render if error is thrown from child', () => {
+    const tree = renderer
+      .create(
+        <ErrorBoundary>
+          <Child />
+        </ErrorBoundary>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should render with all props', () => {
+    const tree = renderer
+      .create(
+        <ErrorBoundaryWithAllProps
+          className="p-4 m-4 font-light"
+          onClick={jest.fn()}
+          errorMessage="Error Message!"
+          reset={{ onReset: jest.fn(), resetKeys: ['Reset Key'] }}
+        >
+          <Child />
+        </ErrorBoundaryWithAllProps>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ webapp/src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,59 @@
+import React, { ReactElement, useMemo } from 'react';
+import { ErrorBoundary as ErrorBounds } from 'react-error-boundary';
+
+export interface ErrorBoundaryProps {
+  children: JSX.Element;
+  errorMessage?: string;
+  // Typically used for resetting state or to re-fetch data.
+  onClick?: () => void;
+  className?: string;
+  // Used for automatically specifiying when the error boundary should reload.
+  reset?: {
+    resetKeys: string[];
+    onReset: () => void;
+  };
+}
+
+const ErrorBoundary = ({
+  children,
+  errorMessage,
+  onClick,
+  className = '',
+  reset,
+}: ErrorBoundaryProps): ReactElement => {
+  const memoizedFallback = useMemo(
+    () => (
+      <div
+        role="alert"
+        className={`bg-red-100 rounded py-2 px-4 flex items-center gap-4 ${className}`}
+      >
+        <b>
+          {errorMessage ||
+            'An error occured! Please try again or contact support if the error persists'}
+        </b>
+        {onClick && (
+          <button
+            onClick={(): void => onClick()}
+            type="button"
+            className="border border-gray-500 rounded px-4 py-2 hover:underline"
+          >
+            Try again
+          </button>
+        )}
+      </div>
+    ),
+    [className, errorMessage, onClick]
+  );
+
+  return (
+    <ErrorBounds
+      fallbackRender={(): JSX.Element => memoizedFallback}
+      resetKeys={reset?.resetKeys}
+      onReset={(): void => reset?.onReset()}
+    >
+      {children}
+    </ErrorBounds>
+  );
+};
+
+export default ErrorBoundary;
