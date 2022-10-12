cf1743964434c5d03f6b4c4e29ed7435a2da1a9b
Benji <benji@beamconnectivity.com>
2022-10-12 12:37:03 +0100

    update checkout

--- webapp/src/pages/Checkout/index.tsx
+++ webapp/src/pages/Checkout/index.tsx
@@ -1,9 +1,34 @@
+import { useFormik } from 'formik';
 import React, { useEffect, useState } from 'react';
 import Button from '../../components/Button';
 import useProduct from '../../hooks/useProduct';
 
 const Checkout = () => {
   const { productInfo } = useProduct();
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      firstLineOfAddress: '',
+      secondLineOfAddress: '',
+      country: '',
+      postcode: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const values = [
+    'First Name',
+    'Last Name',
+    'Email',
+    'First Line of Address',
+    'Second Line of Address',
+    'Country',
+    'Postcode',
+  ];
 
   const truthyDataParsed = productInfo && Object.values(productInfo || '').every((item) => item);
 
@@ -12,42 +37,63 @@ const Checkout = () => {
   if (!productInfo) return <></>;
 
   return (
-    <div className="h-screen">
+    <>
+      <h2 className="text-4xl top-40 border-b w-min mx-auto relative">Checkout</h2>
       {truthyDataParsed ? (
-        <>
-          <img src={productInfo.product?.image} />
-          <div>
-            {productInfo.price}
-            {productInfo.product?.title}
-            {truthyDataParsed && !hovered ? (
-              <Button text="Clear shopping Basket" onClick={() => setHovered(true)} />
-            ) : (
-              <Button
-                text="Are you sure?"
-                onClick={() => {
-                  localStorage.clear();
-                  setHovered(false);
-                }}
-                onMouseLeave={() => {
-                  console.log('called');
-                  setHovered(false);
-                }}
-                navigateTo="/"
-              />
-            )}
-            <p>
-              Note: If you clear your basket, you will not be able to add anything new for 60
-              seconds
-            </p>
+        <div className="flex justify-around divide-x items-center h-screen">
+          <div className="flex flex-col items-center">
+            <p className="underline underline-offset-2">Purchase Information:</p>
+            <img src={productInfo.product?.image} className="mx-8 flex-1" />
+            <div className="flex text-sm divide-x">
+              <p className="px-2">£{productInfo.price}</p>
+              <p className="px-2">{productInfo.product?.title}</p>
+              <p className="px-2">{productInfo.product?.title}</p>
+            </div>
           </div>
-        </>
+          <form className="flex-1 pl-8 flex flex-col gap-8">
+            {Object.entries(formik.values).map(([key, value], idx) => (
+              <>
+                <label htmlFor={key}>{values[idx]}</label>
+                <input
+                  id={key}
+                  name={key}
+                  type={key}
+                  onChange={formik.handleChange}
+                  value={value}
+                  className="w-2/3 bg-transparent outline-none border-0 border-b border-primary-main -mb-4 -mt-8"
+                />
+              </>
+            ))}
+
+            <Button text="Submit" type="submit" classes="mt-4" />
+            <div>
+              {truthyDataParsed && !hovered ? (
+                <Button text="Clear shopping Basket" onClick={() => setHovered(true)} />
+              ) : (
+                <Button
+                  text="Are you sure?"
+                  onClick={() => {
+                    localStorage.clear();
+                    setHovered(false);
+                  }}
+                  onMouseLeave={() => setHovered(false)}
+                  navigateTo="/"
+                />
+              )}
+              <p className="mt-3 italic text-sm underline w-8/12">
+                Note: If you clear your basket, you will not be able to add anything new for 60
+                seconds
+              </p>
+            </div>
+          </form>
+        </div>
       ) : (
         <div className="h-screen w-full flex flex-col gap-4 justify-center items-center text-xl">
           Basket Empty
           <Button text="Continue Shopping" navigateTo="/catalogue" />
         </div>
       )}
-    </div>
+    </>
   );
 };
 
