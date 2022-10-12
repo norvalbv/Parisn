26200bf80d841e5a73eccb64c5fcd110e73a9002
Benji <benji@beamconnectivity.com>
2022-10-12 13:08:47 +0100

    fixes

--- webapp/src/pages/Checkout/index.tsx
+++ webapp/src/pages/Checkout/index.tsx
@@ -1,5 +1,5 @@
 import { useFormik } from 'formik';
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Button from '../../components/Button';
 import useProduct from '../../hooks/useProduct';
 
@@ -36,57 +36,64 @@ const Checkout = () => {
 
   if (!productInfo) return <></>;
 
+  console.log(formik);
+
   return (
     <>
-      <h2 className="text-4xl top-10 border-b w-min mx-auto relative">Checkout</h2>
       {truthyDataParsed ? (
-        <div className="flex justify-around divide-x items-center h-screen">
-          <div className="flex-1 flex flex-col items-center">
-            <p className="underline underline-offset-2">Purchase Information:</p>
-            <img src={productInfo.product?.image} className="mx-8 flex-1" />
-            <div className="flex text-sm divide-x">
-              <p className="px-2">£{productInfo.price}</p>
-              <p className="px-2">{productInfo.product?.title}</p>
-              <p className="px-2">{productInfo.product?.title}</p>
-            </div>
-          </div>
-          <form className="flex-1 pl-8 flex flex-col gap-8">
-            {Object.entries(formik.values).map(([key, value], idx) => (
-              <>
-                <label htmlFor={key}>{values[idx]}</label>
-                <input
-                  id={key}
-                  name={key}
-                  type={key}
-                  onChange={formik.handleChange}
-                  value={value}
-                  className="w-2/3 bg-transparent outline-none border-0 border-b border-primary-main -mb-4 -mt-8"
-                />
-              </>
-            ))}
+        <>
+          <h2 className="text-4xl top-10 border-b w-min mx-auto relative">Checkout</h2>
 
-            <Button text={`Purchase for £${productInfo.price}`} type="submit" classes="mt-4" />
-            <div>
-              {truthyDataParsed && !hovered ? (
-                <Button text="Clear shopping Basket" onClick={() => setHovered(true)} />
-              ) : (
-                <Button
-                  text="Are you sure?"
-                  onClick={() => {
-                    localStorage.clear();
-                    setHovered(false);
-                  }}
-                  onMouseLeave={() => setHovered(false)}
-                  navigateTo="/"
-                />
-              )}
-              <p className="mt-3 italic text-sm underline w-8/12">
-                Note: If you clear your basket, you will not be able to add anything new for 60
-                seconds
-              </p>
+          <div className="flex justify-around divide-x items-center h-screen">
+            <div className="flex-1 flex flex-col items-center">
+              <p className="underline underline-offset-2">Purchase Information:</p>
+              <img src={productInfo.product?.image} className="mx-8 flex-1" />
+              <div className="flex text-sm divide-x">
+                <p className="px-2">£{productInfo.price}</p>
+                <p className="px-2">{productInfo.product?.title}</p>
+                <p className="px-2">{productInfo.product?.title}</p>
+              </div>
             </div>
-          </form>
-        </div>
+            <form className="flex-1 pl-8 flex flex-col gap-8">
+              {Object.entries(formik.initialValues).map(([key], idx) => (
+                <Fragment key={key}>
+                  <label htmlFor={key}>{values[idx]}</label>
+                  <input
+                    id={key}
+                    name={key}
+                    type={key}
+                    onChange={formik.handleChange}
+                    value={Object.values(formik.values)[idx]}
+                    className="w-2/3 bg-transparent outline-none border-0 border-b border-primary-main -mb-4 -mt-8"
+                  />
+                  {Object.values(formik.errors)[idx] ? (
+                    <div>{Object.values(formik.errors)[idx]}</div>
+                  ) : null}
+                </Fragment>
+              ))}
+              <Button text={`Purchase for £${productInfo.price}`} type="submit" classes="mt-4" />
+              <div>
+                {truthyDataParsed && !hovered ? (
+                  <Button text="Clear shopping Basket" onClick={() => setHovered(true)} />
+                ) : (
+                  <Button
+                    text="Are you sure?"
+                    onClick={() => {
+                      localStorage.clear();
+                      setHovered(false);
+                    }}
+                    onMouseLeave={() => setHovered(false)}
+                    navigateTo="/"
+                  />
+                )}
+                <p className="mt-3 italic text-sm underline w-8/12">
+                  Note: If you clear your basket, you will not be able to add anything new for 60
+                  seconds
+                </p>
+              </div>
+            </form>
+          </div>
+        </>
       ) : (
         <div className="h-screen w-full flex flex-col gap-4 justify-center items-center text-xl">
           Basket Empty
