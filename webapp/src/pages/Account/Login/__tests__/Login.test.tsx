5723b49ac5c475a79b9bade43228c0faa6d3ae6c
Benji <benji@beamconnectivity.com>
2022-10-19 20:18:27 +0100

    update snapshots

--- webapp/src/pages/Account/Login/__tests__/Login.test.tsx
+++ webapp/src/pages/Account/Login/__tests__/Login.test.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 import LoginPage from '../';
+import { UserInformationProvider } from '../../../../context/UserContext';
 
 describe('<LoginPage />', () => {
   test('should render correctly', () => {
-    const tree = renderer.create(<LoginPage />).toJSON();
+    const tree = renderer
+      .create(
+        <UserInformationProvider>
+          <BrowserRouter>
+            <LoginPage />
+          </BrowserRouter>
+        </UserInformationProvider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
--- webapp/src/pages/Account/MyAccount/__tests__/MyAccount.test.tsx
+++ webapp/src/pages/Account/MyAccount/__tests__/MyAccount.test.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 import MyAccount from '../';
+import { UserInformationProvider } from '../../../../context/UserContext';
 
 describe('<MyAccount />', () => {
   test('should render correctly', () => {
-    const tree = renderer.create(<MyAccount />).toJSON();
+    const tree = renderer
+      .create(
+        <UserInformationProvider>
+          <BrowserRouter>
+            <MyAccount />
+          </BrowserRouter>
+        </UserInformationProvider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
