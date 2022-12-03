5ce5ddbaa64400a45ed72a6fe9ef72576d756906
Benji <benji@beamconnectivity.com>
2022-12-03 21:12:42 +0000

    remove auth context

--- webapp/src/context/UserContext.tsx
+++ webapp/src/context/UserContext.tsx
@@ -1,5 +1,13 @@
-import React, { createContext, useState, useMemo, useEffect } from 'react';
+import { createContext, useState, useMemo, useEffect } from 'react';
+import { AuthenticationDetails, CognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js';
 import { UserContextInformation, UserInformation } from '../types';
+
+const poolData = {
+  UserPoolId: 'eu-west-2_cuz8GIlYg',
+  ClientId: '3gh7ll3fdvsq6nh642g06emka6',
+};
+
+// const userPool = new CognitoUserPool(poolData);
 
 type ProductContextProviderProps = {
   children?: JSX.Element;
@@ -9,6 +17,35 @@ const UserContext = createContext<UserContextInformation | null>(null);
 
 export const UserInformationProvider = ({ children }: ProductContextProviderProps) => {
   const [user, setUser] = useState<UserInformation | null>(null);
+
+  // const login = async (Username: string, Password: string) => {
+  //   return await new Promise((resolve, reject) => {
+  //     const user = new CognitoUser({
+  //       Username,
+  //       Pool: userPool,
+  //     });
+
+  //     const authDetails = new AuthenticationDetails({
+  //       Username,
+  //       Password,
+  //     });
+
+  //     user.authenticateUser(authDetails, {
+  //       onSuccess: (data) => {
+  //         console.log('on succ', data);
+  //         resolve(data);
+  //       },
+  //       onFailure: (err) => {
+  //         console.error('on fail', err);
+  //         reject(err);
+  //       },
+  //       newPasswordRequired: (data) => {
+  //         console.log('new', data);
+  //         resolve(data);
+  //       },
+  //     });
+  //   });
+  // };
 
   useEffect(() => {
     const retreviedProductInfo = localStorage.getItem('userInformation');
@@ -26,7 +63,7 @@ export const UserInformationProvider = ({ children }: ProductContextProviderProp
       user: { id: user?.id ?? null },
       setUser,
     }),
-    [user]
+    [user, setUser]
   );
 
   return <UserContext.Provider value={memoisedValue}>{children}</UserContext.Provider>;
