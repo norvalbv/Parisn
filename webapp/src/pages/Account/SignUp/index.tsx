8f6da2061ef3ae36cc4613ed6e67b71ed7391214
Benji <benji@beamconnectivity.com>
2022-12-03 17:45:35 +0000

    typo in singup form

--- webapp/src/pages/Account/SignUp/index.tsx
+++ webapp/src/pages/Account/SignUp/index.tsx
@@ -26,7 +26,7 @@ const SignUp = () => {
           email: { initialValue: '', type: 'text', label: 'Email' },
           password: { initialValue: '', type: 'password', label: 'Password' },
         }}
-        submitButtonText="Sign In"
+        submitButtonText="Sign Up"
         submitFn={async (values) => {
           await new Promise((r) => setTimeout(r, 500));
           alert(JSON.stringify(values, null, 2));
