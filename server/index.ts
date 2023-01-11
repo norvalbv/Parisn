62c26a114022ecd4e15784651b8746134ab6dfc2
Benjamin Norval <79381943+norvalbv@users.noreply.github.com>
2023-01-11 18:48:24 +0000

    add basic email templating

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,8 @@ const io = require('socket.io')(server, {
 });
 const bodyParser = require('body-parser');
 require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
@@ -75,7 +77,7 @@ app.get('/collection/:collection', async (req: Request, res: Response) => {
 import { SendEmailCommand } from '@aws-sdk/client-ses';
 import { sesClient } from './AWS/Client';
 
-const createSendEmailCommand = (fromAddress: string) => {
+const createSendEmailCommand = (fromAddress: string, message: string) => {
   return new SendEmailCommand({
     Destination: {
       /* required */
@@ -88,16 +90,16 @@ const createSendEmailCommand = (fromAddress: string) => {
         /* required */
         Html: {
           Charset: 'UTF-8',
-          Data: 'HTML_FORMAT_BODY',
+          Data: message,
         },
         Text: {
           Charset: 'UTF-8',
-          Data: 'TEXT_FORMAT_BODY',
+          Data: 'Hello, Sir!',
         },
       },
       Subject: {
         Charset: 'UTF-8',
-        Data: 'EMAIL_SUBJECT',
+        Data: 'Customer Support Query',
       },
     },
     Source: fromAddress,
@@ -117,7 +119,12 @@ type SendSupportEmail = {
 
 app.post('/send-support-email', async (req: Request<{}, {}, SendSupportEmail>, res: Response) => {
   const body = req.body;
-  const sendEmailCommand = createSendEmailCommand(body.email);
+  const sendEmailCommand = createSendEmailCommand(
+    `${req.body.message} || benjinorval@gmail.com`,
+    `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title></title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; } body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; } table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; } img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; } p { display:block;margin:13px 0; }</style><!--[if mso]> <noscript> <xml> <o:OfficeDocumentSettings> <o:AllowPNG/> <o:PixelsPerInch>96</o:PixelsPerInch> </o:OfficeDocumentSettings> </xml> </noscript> <![endif]--><!--[if lte mso 11]> <style type="text/css"> .mj-outlook-group-fix { width:100% !important; } </style> <![endif]--><style type="text/css">@media only screen and (min-width:480px) { .mj-column-per-100 { width:100% !important; max-width: 100%; } }</style><style media="screen and (min-width:480px)">.moz-text-html .mj-column-per-100 { width:100% !important; max-width: 100%; }</style><style type="text/css"></style></head><body style="word-spacing:normal;"><div><!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]--><div style="margin:0px auto;max-width:600px;"><table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"><tbody><tr><td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"><!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]--><div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"><tbody><tr><td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;"><div style="font-family:helvetica;font-size:20px;line-height:1;text-align:left;color:#F45E43;">${
+      req.body.message || 'Unable to retrieve message!'
+    }</div></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></div></body></html>`
+  );
 
   try {
     const data = await sesClient.send(sendEmailCommand);
