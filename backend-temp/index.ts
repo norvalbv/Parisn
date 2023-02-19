da05ede37e7317b66fb134e0df83675f502bb565
Benjamin Norval <79381943+norvalbv@users.noreply.github.com>
2023-02-19 20:37:46 +0000

    "

---
+++ backend-temp/index.ts
@@ -0,0 +1,60 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3001',
+    credentials: true,
+  },
+});
+require('dotenv').config();
+import { Message } from '../webapp/src/types';
+
+/**
+ * Middleware
+ */
+app.use(cors());
+
+/**
+ * Environment Variables
+ */
+const PORT = process.env.PORT || 8000;
+
+/**
+ * Socket io functions
+ */
+io.on('connect', (socket: any) => {
+  socket.on('join room', (pageParams: string) => {
+    socket.join(pageParams);
+  });
+
+  socket.on('get room count', async (pageParams: string, callback: any) => {
+    const sockets = await io.in(pageParams).allSockets();
+    callback(sockets.size);
+  });
+
+  socket.on('get all room counts', async (pageParams: string, callback: any) => {
+    const sockets = await io.in(pageParams).allSockets();
+    callback(sockets.size);
+  });
+
+  socket.on('chat to room', async (page: string, messageDetails: Message) => {
+    const sockets = await io.in(page).allSockets();
+    io.emit('get chat message from room', messageDetails);
+  });
+
+  let total = 0;
+  socket.on('chat user typing', async (isTyping: boolean) => {
+    if (isTyping) {
+      total++;
+    } else {
+      if (total < 1) return;
+      total--;
+    }
+    io.emit('get chat user typing', total);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
