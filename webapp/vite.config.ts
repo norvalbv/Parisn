486a05afe074ffb6f0f1ba6e4ccd21ef1b9597a7
norvalbv <benjinorval@gmail.com>
2023-03-18 22:31:18 +0000

    fix global?

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-
-declare const global: Window;
+import externals from 'rollup-plugin-node-externals';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  build: {
+    rollupOptions: {
+      external: [
+        'aws-amplify',
+        '@aws-sdk/client-cognito-identity',
+        '@aws-sdk/credential-provider-cognito-identity',
+      ],
+      plugins: [externals()],
+    },
+  },
   define: {
-    global: typeof global === 'undefined' && Window,
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    global: 'window',
   },
 });
