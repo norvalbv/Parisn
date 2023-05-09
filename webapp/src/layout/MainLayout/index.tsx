8de8797f2af0944a31798a270d1e43d7486df63c
norvalbv <benjinorval@gmail.com>
2023-05-09 23:41:12 +0100

    add a ternary for nav display

--- webapp/src/layout/MainLayout/index.tsx
+++ webapp/src/layout/MainLayout/index.tsx
@@ -5,6 +5,7 @@ import MyAccount from 'pages/Account/MyAccount';
 import { useDrawer } from 'hooks/useDrawer';
 import Footer from './Footer';
 import NavBar from './NavBar';
+import Nav from './NavBarHomePage';
 
 // ==============================|| MAIN LAYOUT ||============================== //
 
@@ -24,7 +25,7 @@ const MainLayout = (): ReactElement => {
     <UserInformationProvider>
       <>
         <div className="relative min-h-screen text-gray-300">
-          <NavBar />
+          {location.pathname !== '/home' ? <NavBar /> : <Nav />}
           <Outlet />
           <MyAccount />
         </div>
