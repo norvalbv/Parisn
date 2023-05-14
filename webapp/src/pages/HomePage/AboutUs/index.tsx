c925446e0ad3662291d102a50c3572483ee9c454
norvalbv <benjinorval@gmail.com>
2023-05-14 23:12:00 +0100

    add basic about us section on home page

--- webapp/src/pages/HomePage/AboutUs/index.tsx
+++ webapp/src/pages/HomePage/AboutUs/index.tsx
@@ -1,7 +1,22 @@
+import Radial from 'components/SVG/Design';
 import React, { ReactElement } from 'react';
+import PersonHoldingPhone from 'TempSVGImages/PersonHoldingPhone';
 
 const AboutUs = (): ReactElement => {
-  return <div>AboutUs</div>;
+  return (
+    <div className="flex relative items-center w-1/2 mx-auto gap-8">
+      <Radial />
+      <div>
+        <span className="mb-8 inline-block uppercase">Parisn.com</span>
+        <p>
+          Launched in 2021 allows you to choose the price you pay for your items in a reverse
+          bidding strategy technique. Starting at £1,000 per item, over time the price of the item
+          drops to £0.
+        </p>
+      </div>
+      <PersonHoldingPhone />
+    </div>
+  );
 };
 
 export default AboutUs;
