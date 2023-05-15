733f8a7c290982a1ba185bd6186aefbfb58736fb
norvalbv <benjinorval@gmail.com>
2023-05-15 21:35:35 +0100

    add new clockIcon

---
+++ webapp/src/components/SVG/ClockIcon.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement } from 'react';
+import SVGIcon, { SVGIconProps } from './SVGIcon';
+
+const LeftIcon = (props: SVGIconProps): ReactElement => {
+  return (
+    <SVGIcon width={16} height={16} viewBox="0 0 16 16" fill="#B0B0B0" {...props}>
+      <path
+        d="M8.00004 14.6666C11.682 14.6666 14.6667 11.6819 14.6667 7.99992C14.6667 4.31792 11.682 1.33325 8.00004 1.33325C4.31804 1.33325 1.33337 4.31792 1.33337 7.99992C1.33337 11.6819 4.31804 14.6666 8.00004 14.6666Z"
+        stroke="white"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.00269 4V8.00333L10.829 10.83"
+        stroke="white"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </SVGIcon>
+  );
+};
+
+export default LeftIcon;
--- webapp/src/pages/HomePage/PickedProducts/index.tsx
+++ webapp/src/pages/HomePage/PickedProducts/index.tsx
@@ -1,7 +1,35 @@
 import React, { ReactElement } from 'react';
+import Button from 'components/Button';
 
 const PickedProducts = (): ReactElement => {
-  return <div>PickedProducts</div>;
+  const t = [
+    'https://i.ibb.co/X3fyBCp/ryan-grice-VKDzcs8k-D8-E-unsplash.webp',
+    'https://i.ibb.co/X3fyBCp/ryan-grice-VKDzcs8k-D8-E-unsplash.webp',
+    'https://i.ibb.co/X3fyBCp/ryan-grice-VKDzcs8k-D8-E-unsplash.webp',
+  ];
+  return (
+    <div className="my-10">
+      <div>
+        <div>
+          <span>Dive into Our Handpicked Selection</span>
+          <span>Featured Products with Unmatched Style and Exciting Bidding Opportunities</span>
+        </div>
+      </div>
+      <div className="flex items-center gap-10">
+        {t.map((category, idx) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <section className="h-[31.875rem] w-[19.625rem] relative" key={idx}>
+            <img src={category} alt="p" className="h-full" />
+            <div className="h-[8.25rem] bg-primary-dark/40 absolute z-10 bottom-0 w-full flex flex-col items-center justify-center gap-6">
+              <span>Limited Size Sneakers</span>
+              <Button text="Make A Bid" />
+              <Button text="Buy Now" />
+            </div>
+          </section>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PickedProducts;
