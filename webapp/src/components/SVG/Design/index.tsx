fc1e7350425fe4f61bb34689b4c95ab244aaa587
norvalbv <benjinorval@gmail.com>
2023-05-15 21:28:46 +0100

    add basic categories page, structure

--- webapp/src/components/SVG/Design/index.tsx
+++ webapp/src/components/SVG/Design/index.tsx
@@ -1,21 +1,21 @@
 import React, { ReactElement } from 'react';
 
 type RadialProps = {
-  backgroundColor?: keyof typeof backgroundColorMap;
+  colour?: keyof typeof colourMap;
 };
 
-const Radial = ({ backgroundColor = 'green' }: RadialProps): ReactElement => {
+const Radial = ({ colour = 'green' }: RadialProps): ReactElement => {
   return (
     <svg
       className="absolute w-[25.875rem] h-[62.5rem] -top-40 rotate-[-80deg] blur-[150px]"
       style={{
-        backgroundColor: backgroundColorMap[backgroundColor],
+        backgroundColor: colourMap[colour],
       }}
     />
   );
 };
 
-const backgroundColorMap = {
+const colourMap = {
   purple: '#9E00FF25',
   green: '#00640030',
 };
--- webapp/src/pages/HomePage/Categories/index.tsx
+++ webapp/src/pages/HomePage/Categories/index.tsx
@@ -1,14 +1,37 @@
+import StyledLink from 'components/StyledLink/intex';
+import Radial from 'components/SVG/Design';
 import React, { ReactElement } from 'react';
 
 const Categories = (): ReactElement => {
-  const t = [];
+  const t = [
+    'https://i.ibb.co/X3fyBCp/ryan-grice-VKDzcs8k-D8-E-unsplash.webp',
+    'https://i.ibb.co/X3fyBCp/ryan-grice-VKDzcs8k-D8-E-unsplash.webp',
+    'https://i.ibb.co/X3fyBCp/ryan-grice-VKDzcs8k-D8-E-unsplash.webp',
+  ];
   return (
-    <div>
-      <span>Exceptional Apparel Awaits</span>
-      <span>PARISN&apos;s Fashion Panorama</span>
-      {t.map((category) => (
-        <div className="w-1"></div>
-      ))}
+    <div className="flex flex-col gap-4 items-center relative">
+      <Radial colour="purple" />
+      <span className="block">Exceptional Apparel Awaits</span>
+      <span className="block">PARISN&apos;s Fashion Panorama</span>
+      <div className="flex items-center gap-10">
+        {t.map((category, idx) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <section className="h-[31.875rem] w-[19.625rem] relative" key={idx}>
+            <img src={category} alt="p" className="h-full" />
+            <div className="h-[8.25rem] bg-primary-dark/40 absolute z-10 bottom-0 w-full flex flex-col items-center justify-center gap-6">
+              <span>Limited Size Sneakers</span>
+              <StyledLink to="/" text="View All" />
+            </div>
+          </section>
+        ))}
+      </div>
+      <div className="flex items-center gap-3">
+        <div className="h-0.5 w-[1.875rem] bg-primary-light" />
+        <div className="h-0.5 w-[1.875rem] bg-primary-light/40" />
+        <div className="h-0.5 w-[1.875rem] bg-primary-light/40" />
+        <div className="h-0.5 w-[1.875rem] bg-primary-light/40" />
+        <div className="h-0.5 w-[1.875rem] bg-primary-light/40" />
+      </div>
     </div>
   );
 };
