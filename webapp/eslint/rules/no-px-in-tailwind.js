c4fe657c639c5d23feff573385952bc9a504d5b0
norvalbv <benjinorval@gmail.com>
2023-05-18 22:32:19 +0100

    add custom ESLINT no px rule

---
+++ webapp/eslint/rules/no-px-in-tailwind.js
@@ -0,0 +1,26 @@
+/** @type {import('eslint').Rule.RuleModule} */
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'Disallow usage of `px` in arbitrary Tailwind CSS class names',
+      category: 'Best Practices',
+      recommended: true,
+    },
+    schema: [],
+  },
+  create: (context) => {
+    return {
+      Literal(node) {
+        const value = node.value;
+        const regex = /(\d+)px/;
+        if (typeof value === 'string' && value.match(regex)) {
+          context.report({
+            node,
+            message: 'Usage of `px` is not allowed in arbitrary Tailwind CSS class names',
+          });
+        }
+      },
+    };
+  },
+};
