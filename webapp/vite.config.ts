f2e189c2622a12dc2546b01dac3ef1ce250cc7e2
norvalbv <benjinorval@gmail.com>
2023-06-19 20:58:38 +0100

    remove compression

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -3,14 +3,12 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import { createHtmlPlugin } from 'vite-plugin-html';
-import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
     react(),
     viteTsconfigPaths(),
     EnvironmentPlugin('all'),
-    viteCompression({ algorithm: 'brotliCompress', ext: '.br' }),
     createHtmlPlugin({
       minify: true,
       entry: 'src/index.tsx',
