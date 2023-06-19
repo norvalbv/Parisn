084fde4cf126731d81259871b6c5c9d29d14f160
norvalbv <benjinorval@gmail.com>
2023-06-19 23:39:12 +0100

    fix page incidcators

--- webapp/src/components/PageIndicators/index.tsx
+++ webapp/src/components/PageIndicators/index.tsx
@@ -1,28 +1,31 @@
-import useInterval from 'hooks/useInterval';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import classNames from 'utils/classNames';
+import useInterval from 'hooks/useInterval';
 
 type PageIndicatorsProps = {
   pages: number;
   currentPage: number;
+  setCurrentPage: React.Dispatch<number>;
 };
 
-const PageIndicators = ({ pages, currentPage }: PageIndicatorsProps): ReactElement => {
-  const [page, setPage] = useState(0);
-
+const PageIndicators = ({
+  pages,
+  currentPage,
+  setCurrentPage,
+}: PageIndicatorsProps): ReactElement => {
   useInterval(() => {
-    setPage(page >= pages - 1 ? 0 : page + 1);
-  }, 3500);
+    setCurrentPage(currentPage >= pages - 1 ? 0 : currentPage + 1);
+  }, 10000);
   return (
     <div className="mt-10 flex items-center gap-3">
       {Array(pages)
         .fill(null)
         .map((_, i) => (
-          <div onClick={(): void => setPage(i)} className="h-1 cursor-pointer">
+          <div onClick={(): void => setCurrentPage(i)} className="h-1 cursor-pointer">
             <div
               className={classNames(
                 'h-0.5 w-[1.875rem]',
-                page === i ? 'bg-primary-light' : 'bg-primary-light/40'
+                currentPage === i ? 'bg-primary-light' : 'bg-primary-light/40'
               )}
             />
           </div>
--- webapp/src/pages/HomePage/Categories/index.tsx
+++ webapp/src/pages/HomePage/Categories/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import StyledLink from 'components/StyledLink';
 import Radial from 'components/SVG/Design';
 import { useCollections } from 'services/DataApiService';
@@ -8,11 +8,15 @@ import PageIndicators from 'components/PageIndicators';
 
 const Categories = (): ReactElement => {
   // const { data } = useCollections();
+  const [page, setPage] = useState(0);
   const data = collections;
 
   if (!data) return <Loading />;
 
   const pageCount = Math.ceil(data.length / 3);
+
+  // Create a shallow copy of a data that is limited to three categories per page.
+  const processedData = data.slice(page * 3, page * 3 + 3);
 
   return (
     <section className="relative flex flex-col items-center">
@@ -22,16 +26,12 @@ const Categories = (): ReactElement => {
         <span className="logo-text-medium">PARISN</span>&apos;s Fashion Panorama
       </h3>
       <div className="flex items-center gap-10">
-        {data.map((collection) => (
+        {processedData.map((collection) => (
           <div
             className="relative h-[31.875rem] w-[19.625rem] rounded"
             key={collection.collections}
           >
-            <img
-              src={collection.image}
-              alt="p"
-              className="h-full rounded bg-red-500 brightness-[.65]"
-            />
+            <img src={collection.image} alt="p" className="h-full rounded brightness-[.65]" />
             <div className="absolute bottom-0 z-10 flex h-[8.25rem] w-full flex-col items-center justify-center gap-6 bg-gradient-to-b from-primary-dark/30 via-primary-dark/20 to-primary-dark/20">
               <h4 className="font-thin leading-[1.8125rem]">{collection.collections}</h4>
               <StyledLink to="/" text="View All" />
@@ -39,7 +39,7 @@ const Categories = (): ReactElement => {
           </div>
         ))}
       </div>
-      <PageIndicators pages={pageCount} />
+      <PageIndicators pages={pageCount} currentPage={page} setCurrentPage={setPage} />
     </section>
   );
 };
