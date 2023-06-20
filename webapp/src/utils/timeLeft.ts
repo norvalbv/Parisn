61dcb520a943de6b1989077d4e42eba5ff579d46
norvalbv <benjinorval@gmail.com>
2023-06-20 11:43:19 +0100

    update timeLeft util function

--- webapp/src/utils/timeLeft.ts
+++ webapp/src/utils/timeLeft.ts
@@ -4,15 +4,24 @@ type TimeLeftProps = {
 };
 
 export const timeLeft = ({ startTime, endTime }: TimeLeftProps): string => {
-  const diffInSeconds = endTime - startTime;
+  const diffInMilliseconds = endTime - startTime;
+  const diffInSeconds = diffInMilliseconds / 1000;
+
+  // calculate days
+  const days = Math.floor(diffInSeconds / (3600 * 24));
+  const remainingSecondsAfterDays = diffInSeconds % (3600 * 24);
+
   // calculate hours
-  const hours = Math.floor(diffInSeconds / 3600);
-  // calculate remaining minutes after subtracting the hours
-  const minutes = Math.floor((diffInSeconds % 3600) / 60);
-  // create the output string, making sure to correctly use 'hr' or 'hrs', 'min' or 'mins'
-  const timeleft = `1day ${hours}${hours === 1 ? 'hr' : 'hrs'} ${minutes}${
-    minutes === 1 ? 'min' : 'mins'
-  }`;
+  const hours = Math.floor(remainingSecondsAfterDays / 3600);
+  const remainingSecondsAfterHours = remainingSecondsAfterDays % 3600;
+
+  // calculate minutes
+  const minutes = Math.floor(remainingSecondsAfterHours / 60);
+
+  // create the output string, making sure to correctly use 'day' or 'days', 'hr' or 'hrs', 'min' or 'mins'
+  const timeleft = `${days}${days === 1 ? 'day' : 'days'} ${hours}${
+    hours === 1 ? 'hr' : 'hrs'
+  } ${minutes}${minutes === 1 ? 'min' : 'mins'}`;
 
   return timeleft;
 };
