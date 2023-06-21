d3e75c5ed8cf7327f67acd3ef1ca16758405f48e
norvalbv <benjinorval@gmail.com>
2023-06-21 12:04:22 +0100

    update to scroll hook

--- webapp/src/hooks/useScrollToTop.ts
+++ webapp/src/hooks/useScrollToTop.ts
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+// Scrolls to top of the page unless it's navigating to an anchor tag.
 const useScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
+    if (hash) return;
     window.scrollTo(0, 0);
   }, [pathname]);
 
