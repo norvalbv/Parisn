392bbb32f6bfd7bf6daf137912f5d0166f665e93
norvalbv <benjinorval@gmail.com>
2023-06-27 22:35:03 +0100

    remove amplify

--- webapp/src/aws-exports.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const aws_exports = {
-  region: 'eu-west-2',
-  userPoolId: 'eu-west-2_A1TMcpTAN',
-  userPoolWebClientId: 'ta29c7vqpupkf0t4gbe8e1jaa',
-};
-
-export default aws_exports;
