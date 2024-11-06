2b6484113a19979ac0829f9745073111f9bb7f6d
norvalbv <benjinorval@gmail.com>
2024-11-06 21:06:45 +0000

    fix styling'

--- src/components/PriceDisplay/index.tsx
+++ src/components/PriceDisplay/index.tsx
@@ -15,13 +15,16 @@ const PriceDisplay = ({
 }: PriceDisplayProps): ReactElement => {
   return (
     <Badge
-      className={`h-[6rem] w-max w-72 lg:w-96 md:h-[7.5rem] lg:h-[10rem] flex items-center justify-center ${className}`}
+      className={`h-[6rem] w-72 lg:w-96 md:h-[7.5rem] lg:h-[10rem] flex items-center justify-center ${className}`}
     >
       <div className="flex items-center justify-center">
         <span className="mr-4 text-5xl font-extralight tracking-tighter sm:mr-6 lg:text-[4.5rem]">
           {currency}
         </span>
-        <motion.span className="text-5xl font-extralight tracking-tighter lg:text-[4.5rem]">
+        <motion.span
+          className="text-5xl font-extralight tracking-tighter lg:text-[4.5rem] flex items-center"
+          style={{ minWidth: '5ch' }} // Ensures the price stays in the same position
+        >
           <motion.span style={{ display: 'inline-block' }}>{price}</motion.span>
         </motion.span>
       </div>
